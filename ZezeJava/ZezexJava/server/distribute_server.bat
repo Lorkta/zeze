4528681ad28feea0fccef907b7e69ffe2c51d9f7
YXHY\guoqing.ma <Lorkta@outlook.com>
2024-03-01 18:26:53 +0800

    qq

--- ZezeJava/ZezexJava/server/distribute_server.bat
+++ ZezeJava/ZezexJava/server/distribute_server.bat
@@ -6,7 +6,11 @@ pushd %~dp0
 set classes=../../ZezeJava/build/classes/java/main
 set hotrun=hotrun/*;hotrun/modules/*;hotrun/interfaces/*
 
-echo "Æô¶¯·¢²¼°ü¹ÜÀí·þÎñÆ÷£¬ÓÃÓÚc#.Gen²éÑ¯·¢²¼°üµÄBeanµÄ½á¹¹"
+echo "ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½c#.Genï¿½ï¿½Ñ¯ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Beanï¿½Ä½á¹¹"
 
-java -cp %classes%;%hotrun%;../../ZezeJavaTest/lib/jgrapht-core-1.5.2.jar;../../ZezeJava/lib/* Zeze.Hot.DistributeServer -solution Game
+set JAVA_HOME=C:/Users/guoqing.ma/.jdks/openjdk-21.0.2/
+@rem %JAVA_HOME:"=%
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+
+"%JAVA_EXE%" -cp %classes%;%hotrun%;../../ZezeJavaTest/lib/jgrapht-core-1.5.2.jar;../../ZezeJava/lib/* Zeze.Hot.DistributeServer -solution Game
 
--- ZezeJava/ZezexJava/server/start.bat
+++ ZezeJava/ZezexJava/server/start.bat
@@ -18,11 +18,13 @@ set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
 set zeze_classes=../../ZezeJava/build/classes/java/main;../../ZezeJava/build/resources/main
 
-start "server" "%JAVA_EXE%" -cp %zeze_classes%;./hotrun/server.jar;../../ZezeJavaTest/lib/jgrapht-core-1.5.2.jar;../../ZezeJava/lib/* Program
+"%JAVA_EXE%" -cp %zeze_classes%;./hotrun/server.jar;../../ZezeJavaTest/lib/jgrapht-core-1.5.2.jar;../../ZezeJava/lib/* Program
+
+@rem start "server"
 
 set linkd_classes=%zeze_classes%;../linkd/build/classes/java/main
 start "linkd" "%JAVA_EXE%" -cp %linkd_classes%;../../ZezeJavaTest/lib/jgrapht-core-1.5.2.jar;../../ZezeJava/lib/* Program
 
-timeout 10
-set client_classes=%zeze_classes%;../client/build/classes/java/main
-"%JAVA_EXE%" -cp %client_classes%;../../ZezeJavaTest/lib/jgrapht-core-1.5.2.jar;../../ZezeJava/lib/* Program
+@rem timeout 10
+@rem set client_classes=%zeze_classes%;../client/build/classes/java/main
+@rem "%JAVA_EXE%" -cp %client_classes%;../../ZezeJavaTest/lib/jgrapht-core-1.5.2.jar;../../ZezeJava/lib/* Program
